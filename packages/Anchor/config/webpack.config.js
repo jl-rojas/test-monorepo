a4d296ec92b10fc03442eb3e0bb198b6eb01cc51
Jorge Rojas <jl.rojas@viaducto.io>
2021-03-01 19:37:38 -0600

    aaaaa

--- packages/Anchor/config/webpack.config.js
+++ packages/Anchor/config/webpack.config.js
@@ -11,13 +11,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ["@babel/preset-env",
-              "@babel/preset-react"]
-          }
-        },
+        loader: 'babel-loader',
+        // use: {
+        //   options: {
+        //     presets: ["@babel/preset-env",
+        //       "@babel/preset-react"]
+        //   }
+        // },
       },
       {
         test: /\.css$/,
